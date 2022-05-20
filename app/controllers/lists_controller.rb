3013e854431dd6c9315c6019f09fc4e80f9b2f9d
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-05-20 11:10:08 +0200

    watch list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
   def index
     @lists = List.all
   end
@@ -13,11 +15,20 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
   def list_params
     params.require(:list).permit(:name)
   end
